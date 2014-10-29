c333a252b3818a37f175d1b51339ccdb1ec86624
tomasro27 <tomasrodriguez@utexas.edu>
2014-10-29 15:54:10 -0500

    fixed mapFragment bug #3

--- app/src/main/java/com/rodrigueztomas/timecapsule/MapContainerFragment.java
+++ app/src/main/java/com/rodrigueztomas/timecapsule/MapContainerFragment.java
@@ -5,6 +5,7 @@ package com.rodrigueztomas.timecapsule;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import android.view.ViewGroup;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -26,11 +28,16 @@ public class MapContainerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.map_fragment, container, false);
+
+
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        MapsInitializer.initialize(getActivity().getApplicationContext());
+
         FragmentManager fm = getChildFragmentManager();
 
         if(MainActivity.mapOptions == null)
@@ -42,8 +49,28 @@ public class MapContainerFragment extends Fragment {
         if (fragment == null) {
             fragment = MapFragment.newInstance(MainActivity.mapOptions);
             fm.beginTransaction().add(R.id.map, fragment).commit();
-
         }
+
+
+        MainActivity.map = fragment.getMap();
+
+        if(MainActivity.map != null)
+        {
+            Log.d("debug", "MAp is null");
+            MarkerOptions marker = new MarkerOptions();
+            BitmapDescriptor capsule_icon = BitmapDescriptorFactory.fromResource(R.drawable.capsule_icon);
+            marker.icon(capsule_icon);
+            MainActivity.map.addMarker(marker.position(new LatLng(30.286144, -97.736880)));
+
+            MainActivity.map.addMarker(marker.position(new LatLng(30.286000, -97.736700)));
+
+            MainActivity.map.addMarker(marker.position(new LatLng(30.284000, -97.736600)));
+
+            MainActivity.map.setMyLocationEnabled(true);
+        }
+
+
+
 
 
     }
@@ -56,16 +83,21 @@ public class MapContainerFragment extends Fragment {
 
         }
 
-        MarkerOptions marker = new MarkerOptions();
-        BitmapDescriptor capsule_icon = BitmapDescriptorFactory.fromResource(R.drawable.capsule_icon);
-        marker.icon(capsule_icon);
-         MainActivity.map.addMarker(marker.position(new LatLng(30.286144, -97.736880)));
+        if(MainActivity.map != null)
+        {
+            MarkerOptions marker = new MarkerOptions();
+            BitmapDescriptor capsule_icon = BitmapDescriptorFactory.fromResource(R.drawable.capsule_icon);
+            marker.icon(capsule_icon);
+            MainActivity.map.addMarker(marker.position(new LatLng(30.286144, -97.736880)));
 
-        MainActivity.map.addMarker(marker.position(new LatLng(30.286000, -97.736700)));
+            MainActivity.map.addMarker(marker.position(new LatLng(30.286000, -97.736700)));
 
-        MainActivity.map.addMarker(marker.position(new LatLng(30.284000, -97.736600)));
+            MainActivity.map.addMarker(marker.position(new LatLng(30.284000, -97.736600)));
 
-        MainActivity.map.setMyLocationEnabled(true);
+            MainActivity.map.setMyLocationEnabled(true);
+        }
+
+
 
 
     }
