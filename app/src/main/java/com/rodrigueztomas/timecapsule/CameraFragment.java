8a6726430e6b5e9400f436d48cad3b0bb2b091be
tomasro27 <tomasrodriguez@utexas.edu>
2014-11-12 13:47:33 -0600

    refactored camera fragment

--- app/src/main/java/com/rodrigueztomas/timecapsule/CameraFragment.java
+++ app/src/main/java/com/rodrigueztomas/timecapsule/CameraFragment.java
@@ -1,10 +1,15 @@
 package com.rodrigueztomas.timecapsule;
 
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.app.Instrumentation;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,11 +20,12 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 /**
@@ -27,10 +33,23 @@ import com.google.android.gms.maps.model.MarkerOptions;
  *
  */
 public class CameraFragment extends Fragment {
-    // TODO: change variable names............
-    private Button bt;
-    private ListView lv;
-    private ImageView iv;
+
+    // Activity request codes
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    private static final int CAMERA_CAPTURE_VIDEO_REQUEST_CODE = 200;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    // directory name to store captured images and videos
+    private static final String IMAGE_DIRECTORY_NAME = "Hello Camera";
+
+    private Uri fileUri; // file url to store image/video
+
+    private ImageView imgPreview;
+    private VideoView videoPreview;
+
+
+    private Button btNewCapsule;
     private Button btSaveCapsule;
     private EditText etCapsuleName;
 
@@ -46,14 +65,15 @@ public class CameraFragment extends Fragment {
         View rootView = inflater.inflate(com.rodrigueztomas.timecapsule.R.layout.fragment_camera, container, false);
 
         // Inflate the layout for this fragment
-        iv = (ImageView) rootView.findViewById(R.id.imageView);
-        bt = (Button)rootView.findViewById(R.id.takePhoto);
-        bt.setOnClickListener(new View.OnClickListener() {
+        imgPreview = (ImageView) rootView.findViewById(R.id.imageView);
+        videoPreview = (VideoView) rootView.findViewById(R.id.videoPreview);
+
+
+        btNewCapsule = (Button)rootView.findViewById(R.id.takePhoto);
+        btNewCapsule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
-
+                captureImage();
             }
         });
 
@@ -94,13 +114,6 @@ public class CameraFragment extends Fragment {
 
         return rootView;
     }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == 0) {
-            Bitmap theImage = (Bitmap) data.getExtras().get("data");
-            iv.setImageBitmap(theImage);
-        }
-    }
 
     @Override
     public void onPause()
@@ -108,6 +121,135 @@ public class CameraFragment extends Fragment {
         Log.d("ONPAUSE", "on pause33333");
         super.onPause();
     }
+
+
+
+    /*
+ * Capturing Camera Image will lauch camera app requrest image capture
+ */
+    private void captureImage() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        // start the image capture Intent
+        startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+    }
+
+
+    /**
+     * Creating file uri to store image/video
+     */
+    public Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /*
+     * returning image / video
+     */
+    private static File getOutputMediaFile(int type) {
+
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                IMAGE_DIRECTORY_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
+                        + IMAGE_DIRECTORY_NAME + " directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "IMG_" + timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "VID_" + timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+
+    /**
+     * Receiving activity result method will be called after closing the camera
+     * */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // if the result is capturing Image
+        if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                // successfully captured the image
+                // display it in image view
+                previewCapturedImage();
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                // user cancelled Image capture
+                Toast.makeText(getActivity().getApplicationContext(),
+                        "User cancelled image capture", Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                // failed to capture image
+                Toast.makeText(getActivity().getApplicationContext(),
+                        "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+
+
+    /**
+     * Here we store the file url as it will be null after returning from camera
+     * app
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // save file url in bundle as it will be null on scren orientation
+        // changes
+        outState.putParcelable("file_uri", fileUri);
+    }
+
+    /**
+     * Display image from a path to ImageView
+     */
+    private void previewCapturedImage() {
+        try {
+            // hide video preview
+            videoPreview.setVisibility(View.GONE);
+
+            imgPreview.setVisibility(View.VISIBLE);
+
+            // bimatp factory
+            BitmapFactory.Options options = new BitmapFactory.Options();
+
+            // downsizing image as it throws OutOfMemory Exception for larger
+            // images
+            options.inSampleSize = 8;
+
+            final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(),
+                    options);
+
+            imgPreview.setImageBitmap(bitmap);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/rodrigueztomas/timecapsule/MainActivity.java
+++ app/src/main/java/com/rodrigueztomas/timecapsule/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends Activity {
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
